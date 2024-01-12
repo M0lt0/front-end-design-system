e0956b6bd848290eae29dc65a254e4e6a714607b
M0lt0 <bodymgahed@yahoo.com>
2024-01-12 13:51:18 -0300

    tags design

---
+++ tags/tag-number.blade.php
@@ -0,0 +1,12 @@
+<div class="tag-number">
+    <div class="number"></div>
+</div>
+
+<div class="tag-number">
+    <div class="number"></div>
+    <span class="material-icons-two-tone">
+        all_out
+    </span>
+</div>
+
+
